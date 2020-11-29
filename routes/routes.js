0c18f8be5e0f899a3e265240cf571dcd448bbfa7
mantuonweb <mantuonweb@hotmail.com>
2020-11-29 12:19:32 +0530

    minor changes

--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,5 @@
 const appRouter = (app, fs) => {
-    // variables
+  // variables
   const dataPath = './json-data/campaign.json';
 
   // READ
@@ -10,6 +10,57 @@ const appRouter = (app, fs) => {
       }
 
       res.send(JSON.parse(data));
+    });
+  });
+  app.post('/campaign', (req, res) => {
+    let newCamp = req.body;
+    console.log(newCamp);
+    fs.readFile(dataPath, 'utf8', (err, data) => {
+      if (err) {
+        throw err;
+      }
+      let JSONData = JSON.parse(data);
+      JSONData.push(newCamp);
+      fs.writeFile(dataPath, JSON.stringify(JSONData), function (err) {
+        if (err) throw err;
+        res.send(JSONData);
+      });
+    });
+  });
+  app.delete('/campaign/:id', (req, res) => {
+    let newCamp = req.params.id;
+    fs.readFile(dataPath, 'utf8', (err, data) => {
+      if (err) {
+        throw err;
+      }
+      let JSONData = JSON.parse(data);
+      JSONData = JSONData.filter(item=>{
+        return item.campaignID !== newCamp;
+      })
+      fs.writeFile(dataPath, JSON.stringify(JSONData), function (err) {
+        if (err) throw err;
+        res.send(JSONData);
+      });
+    });
+  });
+  app.put('/campaign', (req, res) => {
+    let newCamp = req.body;
+    fs.readFile(dataPath, 'utf8', (err, data) => {
+      if (err) {
+        throw err;
+      }
+      let JSONData = JSON.parse(data);
+      if(JSONData.find(item=>item.campaignID == newCamp.campaignID)){
+        JSONData = JSONData.map(item=>{
+          return item.campaignID === newCamp.campaignID?newCamp:item;
+        })
+      }else{
+        JSONData.push(newCamp);
+      }
+      fs.writeFile(dataPath, JSON.stringify(JSONData), function (err) {
+        if (err) throw err;
+        res.send(JSONData);
+      });
     });
   });
 };
